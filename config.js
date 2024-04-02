fdab3f37bdc3dd8fa26fc21c8d6044049b02a3f9
©ʍɾ ℂ𝕪𝕓𝕖𝕣 ʍɑղմӀ օƒƒíϲíɑӀ♠️ <157870475+manulofficial7@users.noreply.github.com>
2024-04-02 21:23:12 +0530

    ©ʍɾ ℂ𝕪𝕓𝕖𝕣 ʍɑղմӀ օƒƒíϲíɑӀ♠️

---
+++ config.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+
+// Other
+global.link = 'https://github.com/manulofficial7/Manul-Official-Whatsapp-Bot.git'
+global.linkGroup = 'https://chat.whatsapp.com/EIjQV4nxXwJ6S6QHSa9jpN'
+
+global.apikey = 'bf2d2cf29b3edc604b447983';
+global.ownername = "©ʍɾ ℂ𝕪𝕓𝕖𝕣 ʍɑղմӀ օƒƒíϲíɑӀ♠";
+global.owner = ['94760069363'];
+global.premium = ['94760069363'];
+global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
+global.author = "©ʍɾ ℂ𝕪𝕓𝕖𝕣 ʍɑղմӀ օƒƒíϲíɑӀ♠";
+global.sessionName = 'session';
+global.prefa = [".", "!", "/", "?"];
+global.typemenu = "v1"
+global.menuType = "2"
+global.autoswview = true
+global.autoread = false
+global.available = false
+global.autoTyping = true
+global.autoRecord = false
+global.autoreact = true
+global.onlygroup = false
+global.onlypc = false
+global.welcome = true
+global.autoBlock = true
+
+/*REPLY MESSAGE*/
+global.mess = {
+    banned: '`You are *Banned* fron using commands!`',
+    bangc: '`This Group is *Banned* from using Commands!`',
+    success: '`✓ Success`',
+    admin: '`This feature is only for group admins`',
+    botAdmin: '`I am not an admin!`',
+    owner: '`You are not my owner`',
+    group: '`You can use this command only in groups ❌`',
+    private: '`Feature is used only for private chats!`',
+    bot: '`Bot number user special features`',
+    wait: '*Processing Your request*',
+    endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
+};
+
+global.limitawal = {
+    premium: 'Infinity',
+    free: 10
+};
